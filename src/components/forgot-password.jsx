3f9b345d8b5eaba1409f9c47563b8f5ce782b40e
amanvashishth5 <amanvashishth506@email.com>
2024-06-28 18:14:26 +0530

    change in signup page

---
+++ src/components/forgot-password.jsx
@@ -0,0 +1,27 @@
+import { Form } from "react-bootstrap";
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import "../styles/root.css"
+
+export default function forgotpassword(){
+return<>
+    <Form>
+        <Form.Group as={Col} controlId="formGridEmail">
+        <Form.Label>Please Provide Your Registered Email id to Reset Password</Form.Label>
+        <Form.Control type="text" placeholder="Email id" />
+        </Form.Group>
+
+        <Row>
+        <Button variant="primary" style={{width: '100%'}}>
+        Reset Password
+      </Button>
+
+      <Button variant="primary" style={{width: '100%'}}>
+        Login/SignUp
+      </Button>
+        </Row>
+   
+    </Form>
+</>
+}
--- src/components/signup.jsx
+++ src/components/signup.jsx
@@ -38,51 +38,99 @@ export default function Signup(){
       <Row className="mb-3">
         <Form.Group as={Col} controlId="formGridEmail">
           <Form.Label>First Name</Form.Label>
-          <Form.Control type="text" placeholder="first name" />
+          <Form.Control type="text" placeholder="First Name" />
         </Form.Group>
 
         <Form.Group as={Col} controlId="formGridPassword">
           <Form.Label>Last Name</Form.Label>
-          <Form.Control type="text" placeholder="last name" />
+          <Form.Control type="text" placeholder="Last Name" />
         </Form.Group>
       </Row>
 
-      <Form.Group className="mb-3" controlId="formGridAddress1">
-        <Form.Label>Address</Form.Label>
-        <Form.Control placeholder="1234 Main St" />
+      <Form.Group className="mb-3" controlId="formGridEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control placeholder="Email" />
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formGridAddress2">
-        <Form.Label>Address 2</Form.Label>
-        <Form.Control placeholder="Apartment, studio, or floor" />
+      <Form.Group className="mb-3" controlId="formGridAddress">
+        <Form.Label>Address</Form.Label>
+        <Form.Control placeholder="Address" />
       </Form.Group>
 
       <Row className="mb-3">
-        <Form.Group as={Col} controlId="formGridCity">
-          <Form.Label>City</Form.Label>
-          <Form.Control />
+        <Form.Group as={Col} controlId="formGridCountry">
+          <Form.Label>Country</Form.Label>
+          {/* <Form.Control /> */}
+          <Form.Select defaultValue="Choose...">
+            {/* <option>Choose...</option> */}
+            <option>...</option>
+            </Form.Select>
         </Form.Group>
 
         <Form.Group as={Col} controlId="formGridState">
           <Form.Label>State</Form.Label>
           <Form.Select defaultValue="Choose...">
-            <option>Choose...</option>
+            {/* <option>Choose...</option> */}
+            <option>...</option>
+          </Form.Select>
+        </Form.Group>
+        </Row>
+
+        <Row>
+        <Form.Group as={Col} controlId="formGridCity">
+          <Form.Label>City</Form.Label>
+          <Form.Control />
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridPincode">
+          <Form.Label>Pincode</Form.Label>
+          <Form.Control />
+        </Form.Group>
+     
+      {/* <Form.Group className="mb-3" id="formGridCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group> */}
+      </Row>
+      
+      <Row>
+      <Form.Group as={Col} controlId="formGridISDCode">
+          <Form.Label>ISD Code</Form.Label>
+          <Form.Select defaultValue="Choose...">
+            {/* <option>Choose...</option> */}
             <option>...</option>
           </Form.Select>
         </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridZip">
-          <Form.Label>Zip</Form.Label>
+        <Form.Group as={Col} controlId="formGridMobile">
+          <Form.Label>Mobile Number</Form.Label>
           <Form.Control />
         </Form.Group>
       </Row>
 
-      <Form.Group className="mb-3" id="formGridCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+      <Row>
+      <Form.Group as={Col} controlId="formGridFax">
+          <Form.Label>Fax</Form.Label>
+          <Form.Control placeholder='011-55541234'/>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridPhone">
+          <Form.Label>Phone</Form.Label>
+          <Form.Control placeholder='011-55541234'/>
+        </Form.Group>
+      </Row>
+
+      <Form.Group className="mb-3" controlId="formGroupPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" />
       </Form.Group>
 
-      <Button variant="primary" type="submit">
-        Submit
+      <Form.Group className="mb-3" controlId="formGroupConfirmPassword">
+        <Form.Label>Confirm Password</Form.Label>
+        <Form.Control type="password" />
+      </Form.Group>
+
+      <Button variant="primary" style={{width: '100%'}}>
+        SIGNUP
       </Button>
     </Form>
     </>
