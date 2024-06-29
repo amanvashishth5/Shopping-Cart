9fbf47ed9887fae9eeac0ccc115f21be7ef94c22
Harish Sharma <harish.zetetic@gmail.com>
2024-06-30 03:39:59 +0530

    Login functionality

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,25 +1,39 @@
 
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
-import { Navigate } from 'react-router';
+import { Navigate, useNavigate } from 'react-router';
 import { Link } from 'react-router-dom';
-
+import { useState } from 'react';
+import { getUsers } from '../db';
 export default function Login(){
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
     return <>
     <Form>
       <Form.Group className="mb-3" controlId="formGroupEmail">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" value={email} onChange={e=>setEmail(e.target.value)}/>
       </Form.Group>
       <Form.Group className="mb-3" controlId="formGroupPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" value={password} onChange={e=>setPassword(e.target.value)}/>
       </Form.Group>
 
       <h6 style={{float: 'right'}}>
         <Link to={'/forgot-password'}>Forget Password</Link>
         </h6>
-      <Button variant="primary" style={{width: '100%'}}>
+      <Button variant="primary" style={{width: '100%'}} onClick={async()=>{
+        const allUsers = await getUsers()
+        const isUserExist = allUsers.find((i)=>i.email === email)
+        if(isUserExist){
+          if(isUserExist.password === password){
+            navigate('/products')
+            return;
+          }
+        }
+        alert('Invalid Credentials')
+      }}>
         Login
       </Button>
     </Form>
